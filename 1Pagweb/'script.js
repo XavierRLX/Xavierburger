cd8c04ccac6012718ed8e9a279bc3a7328c19ce4
XavierRLX <renanlima2000.aer@gmail.com>
2021-11-24 13:45:17 -0300

    .

--- 1Pagweb/'script.js
+++ 1Pagweb/'script.js
@@ -2,7 +2,7 @@ function carregar(){
     var aqui = document.getElementById('aqui')
     var data = new Date()
     var hora = data.getHours()
-    if (hora > 0 && hora < 16){
+    if (hora >= 0 && hora < 16){
         aqui.innerHTML = 'fechado'
         aqui.style.background = 'black'
         aqui.style.color = 'white'
