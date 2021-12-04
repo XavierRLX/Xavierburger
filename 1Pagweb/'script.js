9060cc876558838af94de252c799d909ac2b6739
XavierRLX <renanlima2000.aer@gmail.com>
2021-12-04 14:40:34 -0300

    .

--- 1Pagweb/'script.js
+++ 1Pagweb/'script.js
@@ -15,3 +15,28 @@ function carregar(){
 
     }
     }
+    
+
+    var m = document.getElementById('menuf')
+    var x = document.getElementById('menua')
+    var mop = document.getElementById('menu')
+    
+    m.addEventListener('click', abrir)
+    x.addEventListener('click', fechar)
+
+    function abrir(){
+        x.style.display = 'inline'
+        mop.style.display = 'inline'
+        mop.style.transition = '1s'
+        m.style.display = 'none'
+        
+
+    }
+    function fechar(){
+        m.style.display = 'inline'
+        x.style.display = 'none'
+        mop.style.display = 'none'
+        mop.style.transition = '1s'
+
+
+    }
